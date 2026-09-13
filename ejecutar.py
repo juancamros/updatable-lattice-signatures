f2614a8375d2dbbd0421d93547d271eeb5e7ba96
Juan José Campos <juanjosecamposr2103@gmail.com>
2026-09-13 20:29:17 +0200

    Versión actual del proyecto

--- ejecutar.py
+++ ejecutar.py
@@ -10,13 +10,13 @@ import importlib
 
 
 COMMANDS = {
-    "demo": "scripts.demostracion_usgpv",
-    "rendimiento-usgpv": "scripts.rendimiento_usgpv",
-    "rendimiento-crsst21": "scripts.rendimiento_crsst21",
-    "covarianza": "scripts.analisis_covarianza",
-    "pca": "scripts.analisis_pca",
-    "correccion-gaussiana": "scripts.demostracion_correccion_gaussiana",
-    "graficas-dispersion": "scripts.graficas_dispersion",
+    'demo': 'scripts.demostracion_usgpv',
+    'rendimiento-usgpv': 'scripts.rendimiento_usgpv',
+    'rendimiento-crsst21': 'scripts.rendimiento_crsst21',
+    'covarianza': 'scripts.analisis_covarianza',
+    'pca': 'scripts.analisis_pca',
+    'correccion-gaussiana': 'scripts.demostracion_correccion_gaussiana',
+    'graficas-dispersion': 'scripts.graficas_dispersion'
 }
 
 
